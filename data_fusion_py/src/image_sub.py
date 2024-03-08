3e8b9f176378654c8d16f1bcc729ea57e75f755f
Andrew Stirling <andrewipstirling@gmail.com>
2024-03-08 17:52:40 -0500

    Added 4 cameras, ready for kalman

--- data_fusion_py/src/image_sub.py
+++ data_fusion_py/src/image_sub.py
@@ -6,14 +6,19 @@ from matplotlib import pyplot as plt
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import MultiArrayDimension
+from geometry_msgs.msg import PoseWithCovariance
 from cv_bridge import CvBridge, CvBridgeError
 from gazebo_msgs.msg import ModelStates
+import geometry_msgs
 
 PUBRATE = 10
+PLOTTING = False
 
 class ImageSub:
     # Class initialize
-    def __init__(self) -> None:
+    def __init__(self,sub_topic_name:str,pub_topic_name:str) -> None:
         self.show_image = False
         self.img_msg = None
         self.bridge = CvBridge()
@@ -38,7 +43,8 @@ class ImageSub:
         self.encoding_type = 'bgr8'
 
         #create image subscriber object
-        self.sub_image_raw = rospy.Subscriber("/pi_camera/image_raw",Image,self.image_cb) 
+        self.sub_image_raw = rospy.Subscriber(sub_topic_name,Image,self.image_cb) 
+        
 
         # self.pi_camera_info = rospy.Subscriber("/pi_camera/camera_info", CameraInfo, self.camera_info_cb)
         
@@ -86,6 +92,11 @@ class ImageSub:
         self.target_rvel = None
         self.target_corners = None
         self.target_ids = None
+
+        self.pub_pose_estimate = rospy.Publisher(pub_topic_name,PoseWithCovariance,queue_size=1)
+        self.estimated_pose_success = 0
+        self.pose_estimate_msg = PoseWithCovariance()
+
         rospy.loginfo("Succesfully connected" + cv2.__version__)
 
     def pose_cb(self, model_state_msg: ModelStates) -> None:
@@ -106,12 +117,27 @@ class ImageSub:
 
         return 
     
-    # def camera_info_cb(self, cam_msg: CameraInfo) -> None:
-    #     self.camera_matrix = np.array(cam_msg.K)
-    #     np.reshape(self.camera_matrix,(3,3))
-    #     self.cv_cam_mat = cv2.Mat(self.camera_matrix)
-    #     self.dist_coeffs = np.array(cam_msg.D)
-    #     self.cv_dist_coeffs = cv2.Mat(self.dist_coeffs)
+    def publish_pose(self):
+        pos = self.rel_trans.ravel()
+        quat = R.as_quat(R.from_matrix(self.rel_rot_mat)).ravel()
+        
+        succ = np.concatenate((self.std_dev, np.ones(30)*self.estimated_pose_success))
+        cov = list(succ.ravel())
+
+        # print(cov)
+        self.pose_estimate_msg.pose.position.x = pos[0]
+        self.pose_estimate_msg.pose.position.y = pos[1]
+        self.pose_estimate_msg.pose.position.z = pos[2]
+
+        self.pose_estimate_msg.pose.orientation.x = quat[0]
+        self.pose_estimate_msg.pose.orientation.y = quat[1]
+        self.pose_estimate_msg.pose.orientation.z = quat[2]
+        self.pose_estimate_msg.pose.orientation.w = quat[3]
+
+        self.pose_estimate_msg.covariance = cov
+
+        self.pub_pose_estimate.publish(self.pose_estimate_msg)
+        return
         
     def find_markers(self):
         try:
@@ -187,6 +213,7 @@ class ImageSub:
             self.prev_target_tvec = target_tvec
 
             if target_val != 0 and ref_val != 0:
+                self.estimated_pose_success = 1
                 self.rel_trans = target_tvec - ref_tvec
                 # Put into matrix form, return rotation matrix and jacobian of rotation matrix
                 target_rot_mat, _ = cv2.Rodrigues(target_rvec)
@@ -201,9 +228,9 @@ class ImageSub:
 
                 # solvePnp returns rotation of camera relative to marker !!
                 # R_{t//r} = R_{t//c} @ R_{c//r}
-                self.rel_rot = target_rot_mat.T @ ref_rot_mat
+                self.rel_rot_mat = target_rot_mat.T @ ref_rot_mat
 
-                self.rel_rot = R.from_matrix(self.rel_rot).as_euler('xyz',degrees=True)
+                self.rel_rot = R.from_matrix(self.rel_rot_mat).as_euler('xyz',degrees=True)
 
                 _ , tar_jacobian = cv2.projectPoints(target_obj_pts,target_rvec,target_tvec,self.cv_cam_mat,self.cv_dist_coeffs)
 
@@ -216,43 +243,31 @@ class ImageSub:
                 rospy.loginfo_throttle(5,"Tool Pose: \n translation=%s \n rotation=%s", 
                                     self.rel_trans, self.rel_rot)
                 rospy.loginfo_throttle(5,"Standard Deviation [x, y, z]: %s \n", self.std_dev[0:3])
-                rospy.loginfo_throttle(5,cv2.__version__)
-                
-                self.total_distance.append(self.rel_trans)
-                self.total_stddev.append(self.std_dev)
-                self.total_rot.append(self.rel_rot)
-                self.total_ref_state.append(self.gazebo_pos)
+                if PLOTTING:
+                    self.total_distance.append(self.rel_trans)
+                    self.total_stddev.append(self.std_dev)
+                    self.total_rot.append(self.rel_rot)
+                    self.total_ref_state.append(self.gazebo_pos)
                 # self.total_gazebo_ori.append(self.gazebo_ori)
 
             else:
                 rospy.logwarn_throttle(5, "Could not find any markers")
+                self.estimated_pose_success = 0 # False
+                self.rel_trans, self.rel_rot_mat, self.std_dev = np.zeros((3,1)),np.zeros((3,3)),np.zeros((3,1))
 
-            # If drawing
-            if self.show_image:
-                rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners,0.04,
-                                                                    self.cv_cam_mat,
-                                                                    self.cv_dist_coeffs)
-                cv2.aruco.drawDetectedMarkers(cv_image, corners, ids)
-                for i in range(len(ids)):
-                    cv2.drawFrameAxes(cv_image,self.cv_cam_mat,self.cv_dist_coeffs,rvecs[i],tvecs[i],0.02)
         else:
             # rospy.loginfo("Rejected markers: %s",len(rejected))
             rospy.logwarn_throttle(5,"Length of IDs is 0")
+            self.estimated_pose_success = 0 # False
+            self.rel_trans, self.rel_rot_mat, self.std_dev = np.zeros((3,1)),np.zeros((3,3)),np.zeros((3,1))
 
-        if(self.show_image):
-            cv2.imshow("Image Window", cv_image)
-            cv2.waitKey(3)
-        
-        try:
-            self.pub_image.publish(self.bridge.cv2_to_imgmsg(cv_image,self.encoding_type))
-        except CvBridgeError as e:
-            rospy.logwarn(e)
 
 
     def update(self):
         if self.img_msg is not None:
             # rospy.loginfo(self.img_msg.header)
             self.find_markers()
+            self.publish_pose()
             # rospy.loginfo("Completed marker search")
             
         else:
@@ -263,97 +278,96 @@ class ImageSub:
     
 
 def main():
-    image_sub = ImageSub()
+    image_sub_1 = ImageSub(sub_topic_name='/pi_camera_1/image_raw',pub_topic_name='/pi_camera_1/pose_estimate')
 
     rospy.init_node('image_fusion', anonymous=True)
     # Run at 10 Hz
     rospy.Rate(PUBRATE)
 
     while not rospy.is_shutdown():
-        image_sub.update()
+        image_sub_1.update()
     cv2.destroyAllWindows()
     # Plotting 
-    
-    total_distance, total_rot = image_sub.get_dist()
-    total_distance = np.array(total_distance)
-    total_rot = np.array(total_rot)
-    t = np.linspace(0,len(total_distance))
-    const = np.ones_like(total_distance[:,0])
-    true_distance = np.array(image_sub.total_ref_state)
-    true_orientation = np.array(image_sub.total_gazebo_ori)
-    x = total_distance[:,1].flatten()
-    y = total_distance[:,0].flatten()
-    z = total_distance[:,2].flatten()
-    
-    true_x = true_distance[:,0]
-    true_y = true_distance[:,1]
-    true_z = true_distance[:,2] - 0.025
-    print("Measured Len: ", len(z), "Gazebo Len:", len(true_z))
-    plt.tight_layout()
-    plt.subplot(2,1,1)
-    plt.plot(x,color='red',linestyle='--',label='ref x')
-    plt.plot(true_x,color='red',linestyle='-',label='true x')
-    plt.plot(y,color='blue',linestyle='--',label='ref y')
-    plt.plot(true_y,color='blue',linestyle='-',label='true y')
-    plt.plot(z,color='green',linestyle='--',label='ref z')
-    plt.plot(true_z,color='green',linestyle='-',label='true z')
-    plt.title('Marker Position in Reference Frame')
-    plt.ylabel('Position [m]')
-    plt.xlabel('Frames')
-    plt.legend()
+    if PLOTTING:
+        total_distance, total_rot = image_sub_1.get_dist()
+        total_distance = np.array(total_distance)
+        total_rot = np.array(total_rot)
+        t = np.linspace(0,len(total_distance))
+        const = np.ones_like(total_distance[:,0])
+        true_distance = np.array(image_sub_1.total_ref_state)
+        true_orientation = np.array(image_sub_1.total_gazebo_ori)
+        x = total_distance[:,1].flatten()
+        y = total_distance[:,0].flatten()
+        z = total_distance[:,2].flatten()
+        
+        true_x = true_distance[:,0]
+        true_y = true_distance[:,1]
+        true_z = true_distance[:,2] - 0.025
+        plt.tight_layout()
+        plt.subplot(2,1,1)
+        plt.plot(x,color='red',linestyle='--',label='ref x')
+        plt.plot(true_x,color='red',linestyle='-',label='true x')
+        plt.plot(y,color='blue',linestyle='--',label='ref y')
+        plt.plot(true_y,color='blue',linestyle='-',label='true y')
+        plt.plot(z,color='green',linestyle='--',label='ref z')
+        plt.plot(true_z,color='green',linestyle='-',label='true z')
+        plt.title('Marker Position in Reference Frame')
+        plt.ylabel('Position [m]')
+        plt.xlabel('Frames')
+        plt.legend()
 
-    plt.subplot(2,1,2)
-    plt.plot(total_rot[:,0],color='red',linestyle='--',label='roll')
-    # plt.plot(true_orientation[:,0],color='red',linestyle='-')
-    plt.plot(total_rot[:,1],color='blue',linestyle='--',label='pitch')
-    # plt.plot(true_orientation[:,1],color='blue',linestyle='-')
-    plt.plot(total_rot[:,2],color='green',linestyle='--',label='yaw')
-    # plt.plot(true_orientation[:,2],color='green',linestyle='-')
-    plt.title('Marker Rotation in Reference Frame')
-    plt.ylabel('Angular Displacement [deg]')
-    plt.xlabel('Frames')
-    plt.tight_layout()
-    plt.legend()
-    
-    # plt.savefig("/home/astirl/Documents/capstone/figs/marker_pos.png")
-    plt.show()
-    
-    x_err = np.abs(x-true_x)
-    y_err = np.abs(y-true_y)
-    z_err = (z-true_z)
-    x_err_rel = x_err / np.abs(true_x)
-    y_err_rel = y_err / np.abs(true_y)
-    std_dev = np.array(image_sub.total_stddev)
-    plt.subplot(2,1,1)
-    plt.tight_layout()
-    plt.plot(x_err,color='red',linestyle='-',label='x error')
-    plt.plot(y_err,color='blue',linestyle='-',label='y error')
-    # plt.plot(z_err,color='green',linestyle='-',label='z error')
-    plt.title('Absolute Error')
-    plt.ylabel('[m]')
-    plt.xlabel('Frames')
-    plt.legend(loc='upper right')
+        plt.subplot(2,1,2)
+        plt.plot(total_rot[:,0],color='red',linestyle='--',label='roll')
+        # plt.plot(true_orientation[:,0],color='red',linestyle='-')
+        plt.plot(total_rot[:,1],color='blue',linestyle='--',label='pitch')
+        # plt.plot(true_orientation[:,1],color='blue',linestyle='-')
+        plt.plot(total_rot[:,2],color='green',linestyle='--',label='yaw')
+        # plt.plot(true_orientation[:,2],color='green',linestyle='-')
+        plt.title('Marker Rotation in Reference Frame')
+        plt.ylabel('Angular Displacement [deg]')
+        plt.xlabel('Frames')
+        plt.tight_layout()
+        plt.legend()
+        
+        # plt.savefig("/home/astirl/Documents/capstone/figs/marker_pos.png")
+        plt.show()
+        
+        x_err = np.abs(x-true_x)
+        y_err = np.abs(y-true_y)
+        z_err = (z-true_z)
+        x_err_rel = x_err / np.abs(true_x)
+        y_err_rel = y_err / np.abs(true_y)
+        std_dev = np.array(image_sub_1.total_stddev)
+        plt.subplot(2,1,1)
+        plt.tight_layout()
+        plt.plot(x_err,color='red',linestyle='-',label='x error')
+        plt.plot(y_err,color='blue',linestyle='-',label='y error')
+        # plt.plot(z_err,color='green',linestyle='-',label='z error')
+        plt.title('Absolute Error')
+        plt.ylabel('[m]')
+        plt.xlabel('Frames')
+        plt.legend(loc='upper right')
 
-    # plt.subplot(3,1,2)
-    # plt.plot(x_err_rel,color='red',linestyle='-',label='x error')
-    # plt.plot(y_err_rel,color='blue',linestyle='-',label='y error')
-    # plt.title("Relative Error")
-    # plt.ylabel('[%]')
-    # plt.xlabel('Frames')
-    # plt.legend(loc='upper right')
+        # plt.subplot(3,1,2)
+        # plt.plot(x_err_rel,color='red',linestyle='-',label='x error')
+        # plt.plot(y_err_rel,color='blue',linestyle='-',label='y error')
+        # plt.title("Relative Error")
+        # plt.ylabel('[%]')
+        # plt.xlabel('Frames')
+        # plt.legend(loc='upper right')
 
-    plt.subplot(2,1,2)
-    plt.plot(std_dev[:,0],color='red',linestyle='-',label='x std dev')
-    plt.plot(std_dev[:,1],color='blue',linestyle='-',label='y std dev')
-    plt.plot(std_dev[:,2],color='green',linestyle='-',label='z std dev')
-    plt.title('Standard Deviation')
-    plt.ylabel('[pixels]')
-    plt.xlabel('Frames')
-    plt.legend(loc='upper right')
+        plt.subplot(2,1,2)
+        plt.plot(std_dev[:,0],color='red',linestyle='-',label='x std dev')
+        plt.plot(std_dev[:,1],color='blue',linestyle='-',label='y std dev')
+        plt.plot(std_dev[:,2],color='green',linestyle='-',label='z std dev')
+        plt.title('Standard Deviation')
+        plt.ylabel('[pixels]')
+        plt.xlabel('Frames')
+        plt.legend(loc='upper right')
 
-    
-    # plt.savefig("/home/astirl/Documents/capstone/figs/marker_error.png")
-    plt.show()
+        
+        # plt.savefig("/home/astirl/Documents/capstone/figs/marker_error.png")
+        plt.show()
     
     return
 
